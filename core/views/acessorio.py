86ab6c7e33269b37dcfd5e4a03e678505aefd752
Alissonn1325 <alissonfbrito01@gmail.com>
2024-03-26 15:13:17 -0300

    Criação da Api Cor e Marca | Criação da Rest Cor e Marca | Formatação do Código.

--- core/views/acessorio.py
+++ core/views/acessorio.py
@@ -4,6 +4,6 @@ from core.models import Acessorio
 from core.serializers import AcessorioSerializer
 
 
-class AcessorioViewSet (ModelViewSet):
+class AcessorioViewSet(ModelViewSet):
     queryset = Acessorio.objects.all()
     serializer_class = AcessorioSerializer
